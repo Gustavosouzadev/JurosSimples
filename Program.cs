ded68a221743b05a821199b51d7547c92eef5d76
Gustavosouzadev <gustavoduti2003@gmail.com>
2021-03-28 22:51:30 -0300

    Finalizando JurosSimples

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            double j, c, i, t, m;
+    
+            Console.WriteLine("||| Juros simples (j) |||");
+            Console.WriteLine("");
+
+            Console.Write("Capital [c] (R$).......: ");
+            c = Convert.ToDouble (Console.ReadLine());
+            
+            Console.Write("Taxa de juros [i] (%)..: ");
+            i = Convert.ToDouble (Console.ReadLine());
+
+            Console.Write("Tempo [t] (meses)......: ");
+            t = Convert.ToDouble (Console.ReadLine());
+        
+            j = c * (i / 100) * t;
+
+            Console.WriteLine("");
+            Console.WriteLine($"Juros (R$).....: {j:N2} ");
+
+            m = c + j;
+
+            Console.WriteLine($"Montante (R$)..: {m:N2}");
+        }
+    }
+}
